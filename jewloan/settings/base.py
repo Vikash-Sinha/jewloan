c280aa638eb8c2bc9d39bfd9f6c93000c9775c2e
vikash <vikas.s@skysoft.net.in>
2021-09-28 15:15:25 +0530

    update++

--- jewloan/settings/base.py
+++ jewloan/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'lc^9&(j0)k8k2-p-wd(03*4h)fr)5a)i&toviym343)tw9k9%f'
+SECRET_KEY = 'lc^9&(j0)k8k2-p-wd(03*4h)fr)5a)i&toviym343)tw9k9%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
